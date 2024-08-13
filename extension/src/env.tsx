988f7633722b61ce51da3a61a1362b6e039b2e1b
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-08-12 21:18:25 -0700

    readme

--- extension/src/env.tsx
+++ extension/src/env.tsx
@@ -1,2 +1,2 @@
-export const API_SECRET_KEY = "SURFSENSENGvKkWM2VwcIlfFTOz3HR7zB0E4pHFM4llbT3BlbkFJJ0Ood1rgtXbvEXq6Lf"
-export const BACKEND_URL = "https://www.clashforacure.org"
+export const API_SECRET_KEY = ""
+export const BACKEND_URL = ""
