c95e3326b88c7c9800234dc62137fdd01cbad7a3
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-08-22 00:17:46 -0700

    sync updates

--- extension/src/pages/LoginForm.tsx
+++ extension/src/pages/LoginForm.tsx
@@ -82,7 +82,7 @@ export const LoginForm = () => {
                 </div>
                 <button type="submit" disabled={loading} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{loading ? 'Logging in...' : 'Login'}</button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet? <a href="http://localhost:3000/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500" >Sign up</a>
+                  Don’t have an account yet? <a href="https://www.surfsense.net/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500" >Sign up</a>
                 </p>
                 {error && <p style={{ color: 'red' }}>{error}</p>}
               </form>
