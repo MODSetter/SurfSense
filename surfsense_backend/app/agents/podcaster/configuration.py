10d56acaa886ffbe776c8c99bb0430f15e94da32
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2025-05-05 01:39:31 -0700

    feat: Stable & Hella Fast Podcast Agent with auto FFMPEG handling.

---
+++ surfsense_backend/app/agents/podcaster/configuration.py
@@ -0,0 +1,28 @@
+"""Define the configurable parameters for the agent."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, fields
+from typing import Optional
+
+from langchain_core.runnables import RunnableConfig
+
+
+@dataclass(kw_only=True)
+class Configuration:
+    """The configuration for the agent."""
+
+    # Changeme: Add configurable values here!
+    # these values can be pre-set when you
+    # create assistants (https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/)
+    # and when you invoke the graph
+    podcast_title: str 
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> Configuration:
+        """Create a Configuration instance from a RunnableConfig object."""
+        configurable = (config.get("configurable") or {}) if config else {}
+        _fields = {f.name for f in fields(cls) if f.init}
+        return cls(**{k: v for k, v in configurable.items() if k in _fields})
---
+++ surfsense_backend/app/agents/podcaster/graph.py
@@ -0,0 +1,23 @@
+from langgraph.graph import StateGraph
+
+from .configuration import Configuration
+from .state import State
+
+
+from .nodes import create_merged_podcast_audio, create_podcast_transcript
+
+# Define a new graph
+workflow = StateGraph(State, config_schema=Configuration)
+
+# Add the node to the graph
+workflow.add_node("create_podcast_transcript", create_podcast_transcript)
+workflow.add_node("create_merged_podcast_audio", create_merged_podcast_audio)
+
+# Set the entrypoint as `call_model`
+workflow.add_edge("__start__", "create_podcast_transcript")
+workflow.add_edge("create_podcast_transcript", "create_merged_podcast_audio")
+workflow.add_edge("create_merged_podcast_audio", "__end__")
+
+# Compile the workflow into an executable graph
+graph = workflow.compile()
+graph.name = "Surfsense Podcaster"  # This defines the custom name in LangSmith
