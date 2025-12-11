fc08f721b0de018e32fb0a778cd9b7a52b8de6c7
CREDO23 <thierrybakera12@gmail.com>
2025-12-11 21:00:17 +0000

    feat(search-spaces): add communityPromptsAtom and remove obvious comments

--- surfsense_web/atoms/search-spaces/search-space-query.atoms.ts
+++ surfsense_web/atoms/search-spaces/search-space-query.atoms.ts
@@ -4,24 +4,32 @@ import type { GetSearchSpacesRequest } from "@/contracts/types/search-space.type
 import { searchSpacesApiService } from "@/lib/apis/search-spaces-api.service";
 import { cacheKeys } from "@/lib/query-client/cache-keys";
 
-// Atom to store current query params for search spaces
 export const searchSpacesQueryParamsAtom = atom<GetSearchSpacesRequest["queryParams"]>({
 	skip: 0,
 	limit: 10,
 	owned_only: false,
 });
 
-// Query atom to fetch search spaces with query params
 export const searchSpacesAtom = atomWithQuery((get) => {
 	const queryParams = get(searchSpacesQueryParamsAtom);
 
 	return {
 		queryKey: cacheKeys.searchSpaces.withQueryParams(queryParams),
-		staleTime: 5 * 60 * 1000, // 5 minutes
+		staleTime: 5 * 60 * 1000,
 		queryFn: async () => {
 			return searchSpacesApiService.getSearchSpaces({
 				queryParams,
 			});
+		},
+	};
+});
+
+export const communityPromptsAtom = atomWithQuery(() => {
+	return {
+		queryKey: cacheKeys.searchSpaces.communityPrompts,
+		staleTime: 30 * 60 * 1000,
+		queryFn: async () => {
+			return searchSpacesApiService.getCommunityPrompts();
 		},
 	};
 });
