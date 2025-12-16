22f8d5bbbb5cf6509fd493c5ed83e6c708eb803d
CREDO23 <thierrybakera12@gmail.com>
2025-12-16 13:42:12 +0000

    feat: add updateMember method to members API service

--- surfsense_web/lib/apis/members-api.service.ts
+++ surfsense_web/lib/apis/members-api.service.ts
@@ -3,6 +3,9 @@ import {
 	type GetMembersRequest,
 	getMembersRequest,
 	getMembersResponse,
+	type UpdateMembershipRequest,
+	updateMembershipRequest,
+	updateMembershipResponse,
 } from "@/contracts/types/members.types";
 
 class MembersApiService {
@@ -14,6 +17,20 @@ class MembersApiService {
 		return baseApiService.get(
 			`/searchspaces/${parsedRequest.search_space_id}/members`,
 			getMembersResponse,
+		);
+	}
+
+	/**
+	 * Update a member's role
+	 */
+	async updateMember(request: UpdateMembershipRequest) {
+		const parsedRequest = updateMembershipRequest.parse(request);
+		return baseApiService.put(
+			`/searchspaces/${parsedRequest.search_space_id}/members/${parsedRequest.membership_id}`,
+			updateMembershipResponse,
+			{
+				body: parsedRequest.data,
+			},
 		);
 	}
 }
