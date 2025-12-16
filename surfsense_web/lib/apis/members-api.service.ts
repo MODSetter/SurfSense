eb38a0277562de4df6c003b263d685bca7e4826a
CREDO23 <thierrybakera12@gmail.com>
2025-12-16 14:13:53 +0000

    feat: add deleteMember method to members API service

--- surfsense_web/lib/apis/members-api.service.ts
+++ surfsense_web/lib/apis/members-api.service.ts
@@ -3,10 +3,14 @@ import {
 	type GetMembersResponse,
 	type UpdateMembershipRequest,
 	type UpdateMembershipResponse,
+	type DeleteMembershipRequest,
+	type DeleteMembershipResponse,
 	getMembersRequest,
 	getMembersResponse,
 	updateMembershipRequest,
 	updateMembershipResponse,
+	deleteMembershipRequest,
+	deleteMembershipResponse,
 } from "@/contracts/types/members.types";
 import { ValidationError } from "@/lib/error";
 import { baseApiService } from "./base-api.service";
@@ -50,6 +54,25 @@ class MembersApiService {
 			{
 				body: parsedRequest.data.data,
 			},
+		);
+	};
+
+	/**
+	 * Delete a member from search space
+	 */
+	deleteMember = async (request: DeleteMembershipRequest) => {
+		const parsedRequest = deleteMembershipRequest.safeParse(request);
+
+		if (!parsedRequest.success) {
+			console.error("Invalid request:", parsedRequest.error);
+
+			const errorMessage = parsedRequest.error.errors.map((err) => err.message).join(", ");
+			throw new ValidationError(`Invalid request: ${errorMessage}`);
+		}
+
+		return baseApiService.delete(
+			`/searchspaces/${parsedRequest.data.search_space_id}/members/${parsedRequest.data.membership_id}`,
+			deleteMembershipResponse,
 		);
 	};
 }
