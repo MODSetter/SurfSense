7192bb1a85eeed826004aa51c10e9099d4a174e0
CREDO23 <thierrybakera12@gmail.com>
2026-02-06 14:49:40 +0200

    Skip auth on public routes

--- surfsense_web/atoms/user/user-query.atoms.ts
+++ surfsense_web/atoms/user/user-query.atoms.ts
@@ -1,16 +1,14 @@
 import { atomWithQuery } from "jotai-tanstack-query";
 import { userApiService } from "@/lib/apis/user-api.service";
-import { getBearerToken } from "@/lib/auth-utils";
+import { getBearerToken, isPublicRoute } from "@/lib/auth-utils";
 import { cacheKeys } from "@/lib/query-client/cache-keys";
 
 export const currentUserAtom = atomWithQuery(() => {
+	const pathname = typeof window !== "undefined" ? window.location.pathname : null;
 	return {
 		queryKey: cacheKeys.user.current(),
 		staleTime: 5 * 60 * 1000, // 5 minutes
-		// Only fetch user data when a bearer token is present
-		enabled: !!getBearerToken(),
-		queryFn: async () => {
-			return userApiService.getMe();
-		},
+		enabled: !!getBearerToken() && pathname !== null && !isPublicRoute(pathname),
+		queryFn: async () => userApiService.getMe(),
 	};
 });
