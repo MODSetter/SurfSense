c9b0e55ce9aa66d4b67d7a36eef51a22ff1ae7a2
CREDO23 <thierrybakera12@gmail.com>
2026-02-12 15:21:18 +0200

    add notion tool metadata service

---
+++ surfsense_backend/app/services/notion/__init__.py
@@ -0,0 +1,11 @@
+from app.services.notion.tool_metadata_service import (
+    NotionAccount,
+    NotionPage,
+    NotionToolMetadataService,
+)
+
+__all__ = [
+    "NotionAccount",
+    "NotionPage",
+    "NotionToolMetadataService",
+]
