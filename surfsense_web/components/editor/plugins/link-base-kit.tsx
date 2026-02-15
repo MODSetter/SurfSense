73147c69a330ebcc22420d7bf910a847c3b1c635
Anish Sarkar <104695310+AnishSarkar22@users.noreply.github.com>
2026-02-16 01:15:21 +0530

    refactor: remove deprecated editor plugins and UI components for cleaner codebase

--- surfsense_web/components/editor/plugins/link-base-kit.tsx
+++
@@ -1,5 +0,0 @@
-import { BaseLinkPlugin } from '@platejs/link';
-
-import { LinkElementStatic } from '@/components/ui/link-node-static';
-
-export const BaseLinkKit = [BaseLinkPlugin.withComponent(LinkElementStatic)];
