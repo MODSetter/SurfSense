58955cdc9fb9adedbe45583cc2fccbc979839962
CREDO23 <thierrybakera12@gmail.com>
2026-02-18 17:51:53 +0200

    feat(linear): add LinearKBSyncService and services __init__

---
+++ surfsense_backend/app/services/linear/__init__.py
@@ -0,0 +1,13 @@
+from app.services.linear.kb_sync_service import LinearKBSyncService
+from app.services.linear.tool_metadata_service import (
+    LinearIssue,
+    LinearToolMetadataService,
+    LinearWorkspace,
+)
+
+__all__ = [
+    "LinearIssue",
+    "LinearKBSyncService",
+    "LinearToolMetadataService",
+    "LinearWorkspace",
+]
