0e19d1ff20d2eb0af89c7542318c355ec99118a2
JoeMakuta <makutajosue@gmail.com>
2026-03-26 18:14:20 +0200

    feat: Add global error handling

--- surfsense_web/app/global-error.tsx
+++ surfsense_web/app/global-error.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import "./globals.css";
-
-import NextError from "next/error";
 import posthog from "posthog-js";
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
